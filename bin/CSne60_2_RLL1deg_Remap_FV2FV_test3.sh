0320a9a5149d54bb20854726914bb25f1de7bf85
Jorge E Guerra <jeguerra@ucdavis.edu>
2019-06-13 10:38:09 -0700

    Test data updates.

--- bin/CSne60_2_RLL1deg_Remap_FV2FV_test3.sh
+++ bin/CSne60_2_RLL1deg_Remap_FV2FV_test3.sh
@@ -1,7 +1,7 @@
-# Generate the mapping data for testing CSne30 to RLL1deg in FV2FV
+# Generate the mapping data for testing CSne60 to RLL1deg in FV2FV
 source envs.sh
-$TEMPESTREMAP_DIR/./GenerateOverlapMesh --a ../meshes/outCSne30.g --b ../meshes/outRLL1deg.g --out ../meshes/CSne30_2_RLL1deg.g
-$TEMPESTREMAP_DIR/./GenerateOfflineMap --in_mesh ../meshes/outCSne30.g --out_mesh ../meshes/outRLL1deg.g --ov_mesh ../meshes/CSne30_2_RLL1deg.g --in_np 4 --out_map ../meshes/CSne30_2_RLL1deg.nc
+$TEMPESTREMAP_DIR/./GenerateOverlapMesh --a ../meshes/outCSne60.g --b ../meshes/outRLL1deg.g --out ../meshes/CSne60_2_RLL1deg.g
+$TEMPESTREMAP_DIR/./GenerateOfflineMap --in_mesh ../meshes/outCSne60.g --out_mesh ../meshes/outRLL1deg.g --ov_mesh ../meshes/CSne60_2_RLL1deg.g --in_np 4 --out_map ../meshes/CSne60_2_RLL1deg.nc
 
-# Remap test 3 from CSne30 to RLL1deg
-$TEMPESTREMAP_DIR/./ApplyOfflineMap --map ../meshes/CSne30_2_RLL1deg.nc --var Psi --in_data ../testdata_CSne30_np4_3.nc --out_data ../testdata_outCSne30_2_RLL1deg_np4_3.nc
+# Remap test 3 from CSne60 to RLL1deg
+$TEMPESTREMAP_DIR/./ApplyOfflineMap --map ../meshes/CSne60_2_RLL1deg.nc --var Psi --in_data ../testdata_CSne60_np4_3.nc --out_data ../testdata_outCSne60_2_RLL1deg_np4_3.nc
