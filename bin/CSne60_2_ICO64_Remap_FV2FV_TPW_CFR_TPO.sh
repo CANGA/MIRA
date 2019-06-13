c006ee3c22a54a5ad1cacfa870882ea42c930b3d
Jorge E Guerra <jeguerra@ucdavis.edu>
2019-06-13 10:39:12 -0700

    Test data cleanups.

--- bin/CSne60_2_ICO64_Remap_FV2FV_TPW_CFR_TPO.sh
+++ bin/CSne60_2_ICO64_Remap_FV2FV_TPW_CFR_TPO.sh
@@ -1,11 +1,11 @@
-# Generate the mapping data for testing CSne30 to ICO64
+# Generate the mapping data for testing CSne60 to ICO64
 source envs.sh
-$TEMPESTREMAP_DIR/GenerateOverlapMesh --a outCSne30.g --b outICO64.g --out CSne30_2_ICO64.g
-$TEMPESTREMAP_DIR/GenerateOfflineMap --in_mesh outCSne30.g --out_mesh outICO64.g --ov_mesh CSne30_2_ICO64.g --in_np 4 --out_map CSne30_2_ICO64_np4_TPW_CFR_TPO.nc
+$TEMPESTREMAP_DIR/GenerateOverlapMesh --a outCSne60.g --b outICO64.g --out CSne60_2_ICO64.g
+$TEMPESTREMAP_DIR/GenerateOfflineMap --in_mesh outCSne60.g --out_mesh outICO64.g --ov_mesh CSne60_2_ICO64.g --in_np 4 --out_map CSne60_2_ICO64_np4_TPW_CFR_TPO.nc
 
-# Remap TPW from CSne30 to ICO64
-$TEMPESTREMAP_DIR/ApplyOfflineMap --map CSne30_2_ICO64_np4_TPW_CFR_TPO.nc --var TotalPrecipWater --in_data testdata_outCSne30_TPW_CFR_TPO.nc --out_data testdata_outCSne30_2_ICO64_TPW.nc
-# Remap CFR from CSne30 to ICO64
-$TEMPESTREMAP_DIR/ApplyOfflineMap --map CSne30_2_ICO64_np4_TPW_CFR_TPO.nc --var CloudFraction --in_data testdata_outCSne30_TPW_CFR_TPO.nc --out_data testdata_outCSne30_2_ICO64_CFR.nc
-# Remap TPO from CSne30 to ICO64
-$TEMPESTREMAP_DIR/ApplyOfflineMap --map CSne30_2_ICO64_np4_TPW_CFR_TPO.nc --var Topography --in_data testdata_outCSne30_TPW_CFR_TPO.nc --out_data testdata_outCSne30_2_ICO64_TPO.nc
+# Remap TPW from CSne60 to ICO64
+$TEMPESTREMAP_DIR/ApplyOfflineMap --map CSne60_2_ICO64_np4_TPW_CFR_TPO.nc --var TotalPrecipWater --in_data testdata_outCSne60_TPW_CFR_TPO.nc --out_data testdata_outCSne60_2_ICO64_TPW.nc
+# Remap CFR from CSne60 to ICO64
+$TEMPESTREMAP_DIR/ApplyOfflineMap --map CSne60_2_ICO64_np4_TPW_CFR_TPO.nc --var CloudFraction --in_data testdata_outCSne60_TPW_CFR_TPO.nc --out_data testdata_outCSne60_2_ICO64_CFR.nc
+# Remap TPO from CSne60 to ICO64
+$TEMPESTREMAP_DIR/ApplyOfflineMap --map CSne60_2_ICO64_np4_TPW_CFR_TPO.nc --var Topography --in_data testdata_outCSne60_TPW_CFR_TPO.nc --out_data testdata_outCSne60_2_ICO64_TPO.nc
