6746063f6021934d3bd650015ee0b2ede9bced3c
Vijay Mahadevan <vijay.m@gmail.com>
2020-04-01 19:57:10 -0400

    Add a flag to force recomputation of areas and adjacencies, even if available already in the mesh file

--- CANGAMeshPreprocessDriver.py
+++ CANGAMeshPreprocessDriver.py
@@ -39,12 +39,14 @@ def parseCommandLine(argv):
        SCRIPwithoutConn = False
        SCRIPwithConn = False
        SpectralElement = False
+       forceRecompute = False
        # Polynomial order for spectral elements
        seOrder = 4
        
        try:
               opts, args = getopt.getopt(argv, 'hv:', \
-                                        ['mesh=',
+                                        ['mesh=', \
+                                         'force', \
                                          'ExodusSingleConn', 'SCRIPwithoutConn', \
                                          'SCRIPwithConn', 'SpectralElement', 'seorder='])
        except getopt.GetoptError:
@@ -53,6 +55,7 @@ def parseCommandLine(argv):
                     '--mesh <meshFile>', \
                     '--<meshConfiguration>', \
                     '--<makeSEGrid>', \
+                    '--force', \
                     '--seorder <polyOrder>')
               sys.exit(2)
               
@@ -68,6 +71,8 @@ def parseCommandLine(argv):
                      sys.exit()
               elif opt == '--mesh':
                      sampleMesh = arg
+              elif opt == '--force':
+                     forceRecompute = True
               elif opt == '--ExodusSingleConn':
                      ExodusSingleConn = True
               elif opt == '--SCRIPwithoutConn':
@@ -105,7 +110,7 @@ def parseCommandLine(argv):
               sys.exit(2)
        
        return sampleMesh, ExodusSingleConn, SCRIPwithoutConn, SCRIPwithConn, \
-              SpectralElement, seOrder
+              SpectralElement, seOrder, forceRecompute
 
 if __name__ == '__main__':
        print('Welcome to CANGA remapping intercomparison mesh pre-processor!')
@@ -113,7 +118,7 @@ if __name__ == '__main__':
        
        # Parse the commandline! COMMENT OUT TO RUN IN IDE
        mesh_file, ExodusSingleConn, SCRIPwithoutConn, SCRIPwithConn, \
-       SpectralElement, seOrder = parseCommandLine(sys.argv[1:])
+       SpectralElement, seOrder, forceRecompute = parseCommandLine(sys.argv[1:])
 
        # Set the names for the auxiliary area and adjacency maps (NOT USER)
        varAreaName = 'cell_area'
@@ -245,7 +250,7 @@ if __name__ == '__main__':
               varCoord = sphcrt.computeLL2Cart(varCoordLL)
               varCoord = varCoord.T
               
-              try:   
+              try:
                      print('Storing connectivity and coordinate arrays from raw SCRIP')
                      meshFileOut = m_fid.createDimension(numVerts, np.size(varCoord, 1))
                      meshFileOut = m_fid.createDimension(numDims, 3)
@@ -260,6 +265,7 @@ if __name__ == '__main__':
               
        # Close the mesh file
        m_fid.close()
+
        # Open the mesh file for new data
        m_fid = Dataset(outFileName, 'a')
               
@@ -268,9 +274,12 @@ if __name__ == '__main__':
        start = time.time()
        print('Computing adjacency maps...')
        
-       if not m_fid.variables[varAdjaName]:
+       if not m_fid.variables[varAdjaName] or forceRecompute:
               try:
-                     meshFileOut = m_fid.createVariable(varAdjaName, 'i4', (numCells, numEdges))
+                     if not m_fid.variables[varAdjaName]:
+                            meshFileOut = m_fid.createVariable(varAdjaName, 'i4', (numCells, numEdges))
+                     else:
+                            meshFileOut = m_fid.variables[varAdjaName]
 
                      print('Adjacency data computed/written to mesh file for the first time...')
                      # Compute adjacency maps for both meshes (source stencil NOT needed)
@@ -291,15 +300,19 @@ if __name__ == '__main__':
        start = time.time()
        print('Computing mesh areas...')
        
-       if not m_fid.variables[varAreaName]:
+       if not m_fid.variables[varAreaName] or forceRecompute:
 
               try:
-                     meshFileOut = m_fid.createVariable(varAreaName, 'f8', (numCells, ))
+                     NEL = len(varCon)
+                     if not m_fid.variables[varAreaName]:
+                            meshFileOut = m_fid.createVariable(varAreaName, 'f8', (numCells, ))
+                            area = np.zeros((NEL))
+                     else:
+                            meshFileOut = m_fid.variables[varAreaName]
+                            area = np.zeros(meshFileOut.shape)
 
                      print('Cell areas computed/written to mesh file for the first time...')
                      # Precompute the area weights and then look them up in the integral below
-                     NEL = len(varCon)
-                     area = np.zeros((NEL,1))
                      for ii in range(NEL):
                             cdex = varCon[ii,:] - 1
                             thisCell = varCoord[:,cdex]
@@ -307,7 +320,7 @@ if __name__ == '__main__':
 
                      area = np.ravel(area)
 
-                     meshFileOut[:] = area
+                     meshFileOut[:] = area[:]
               except RuntimeError:
                      print('Source areas already exist in mesh data file.')
        else:
