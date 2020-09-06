37d8e730858a1dc82354ccb6277b9f7a96190b34
Vijay Mahadevan <vijay.m@gmail.com>
2020-09-06 15:42:01 -0400

    Modify the global extrema metrics to follow similar definition as the local extrema

--- computeGlobalExtremaMetrics.py
+++ computeGlobalExtremaMetrics.py
@@ -10,10 +10,11 @@ Computes the global extrema metrics for regridded and reference target data
 
 import numpy as np
 
+
 def computeGlobalExtremaMetrics(varS2T, varST):
-       
-       Lden = (np.max(abs(varST)) - np.min(abs(varST)))
-       L_min = np.abs(np.amin(varST) - np.amin(varS2T)) / Lden
-       L_max = np.abs(np.amax(varS2T) - np.amax(varST)) / Lden
-       
-       return L_min, L_max
+
+    Lden = (np.max(abs(varST)) - np.min(abs(varST)))
+    L_min = min(np.amin(varS2T) - np.amin(varST), 0.0) / Lden  # < 0 indicates failure
+    L_max = max(np.amax(varS2T) - np.amax(varST), 0.0) / Lden  # > 0 indicates failure
+
+    return L_min, L_max
